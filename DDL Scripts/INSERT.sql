f40f44ca720836dbd441a278b886fcdb2e9de60d
Sonali Patel <sonalipatel23600@gmail.com>
2020-12-13 14:04:46 +0530

    completed readData

---
+++ DDL Scripts/INSERT.sql
@@ -0,0 +1,16 @@
+-- INSERT INTO reader
+
+INSERT INTO reader(username,email,password,profile_img,Fname,Lname,city,state) 
+VALUES ('jdoe','jdoe@gmail.com','abc123','https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80','John','Doe','Indore','Madhya Pradesh');
+
+INSERT INTO reader(username,email,password,profile_img,Fname,Lname,city,state) 
+VALUES ('tom123','tom@gmail.com','abc123','https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80','Tom','Smith','Bhopal','Madhya Pradesh');
+
+INSERT INTO reader(username,email,password,profile_img,Fname,Lname,city,state) 
+VALUES ('jlilly','lilly@gmail.com','abc123','https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80','Lily','Jae','Ujjain','Madhya Pradesh');
+
+INSERT INTO reader(username,email,password,profile_img,Fname,Lname,city,state) 
+VALUES ('nobody777','james@gmail.com','abc123','https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80','James','Ray','Jaipur','Rajasthan');
+
+INSERT INTO reader(username,email,password,profile_img,Fname,Lname,city,state) 
+VALUES ('sara99','sara99@gmail.com','abc123','https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80','Sara','Paulson','Mumbai','Maharashtra');
--- node/app.js
+++ node/app.js
@@ -25,3 +25,20 @@ conn.connect(
 	}	
 });
 
+//Read DATA
+function readData(){
+	conn.query('SELECT * FROM reader', 
+		function (err, results, fields) {
+			if (err) throw err;
+			else console.log('Selected ' + results.length + ' row(s).');
+			for (i = 0; i < results.length; i++) {
+				console.log('Row: ' + JSON.stringify(results[i]));
+			}
+			console.log('Done.');
+		})
+   conn.end(
+	   function (err) { 
+			if (err) throw err;
+			else  console.log('Closing connection.') 
+	});
+};
