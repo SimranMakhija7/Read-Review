e53125323a03b3e2a24602b0cbeb959603bb55fd
Sonali Patel <sonalipatel23600@gmail.com>
2020-12-17 14:25:54 +0530

    created session cookie

--- node/app.js
+++ node/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mysql = require('mysql');
 const dotenv  = require('dotenv');
+const cookieParser = require('cookie-parser');
 
 dotenv.config({ path:'./.env' });
 const app = express();
@@ -23,6 +24,7 @@ app.use(express.static(publicDirectory));
 //Parese url-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+app.use(cookieParser());
 
 app.set('view engine','hbs');
 
--- node/controllers/auth.js
+++ node/controllers/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
 
 const dotenv  = require('dotenv');
 
@@ -67,8 +68,24 @@ exports.login = async (req,res) => {
                     })  
                 }
             }
+                const username = results[0].username;
+                const token = jwt.sign({username: username},process.env.JWT_SECRET,{
+                    expiresIn: process.env.JWT_EXPIRES_IN
+                });
+
+                console.log("token"+token);
+                const cookieOptions = {
+                    expires: new Date(
+                        Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+                    ),
+                    httpOnly: true
+                }
+
+                res.cookie('jwt',token,cookieOptions);
             
-            res.status(200).redirect("/user/" + username);
+                res.status(200).redirect("/user/" + username);
+
+            
             
             
         })
